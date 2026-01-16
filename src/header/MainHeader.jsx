6603c72b89b6c6f5a21bb88776d408a518cf7bb9
chominseo <singer100@yu.ac.kr>
2026-01-16 17:53:33 +0900

    feat : 기본 Header UI 구현

--- src/header/MainHeader.jsx
+++ src/header/MainHeader.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Logo from "./../assets/Logo.svg"
 const MainHeader = () => {
   return (
-    <div className=''>MainHeader</div>
+    <div className='flex flex-row font-pretendard py-9 items-center justify-between'>
+        <img className='pl-10' src={Logo}/>
+            <div className='flex flex-row gap-10 items-center'>
+                <span className='text-[20px] text-[#686E7D] '>HOME</span>
+                <span className='text-[20px] text-[#686E7D] '>RECTUIT</span>
+                <span className='text-[20px] text-[#686E7D] '>PROJECT</span>
+                <span className='text-[20px] text-[#686E7D] mr-5'>Q&A</span>
+                <div className='border rounded-[40px] px-10 py-3 font-extrabold text-white text-[20px] bg-gradient-to-r
+    from-[#FF5E00]/40
+    to-[#FFAE00]/40
+    border-white/30
+    backdrop-blur-sm mr-15'>14기 지원하기</div>
+            </div>
+        
+    </div>
   )
 }
 
--- src/pages/MainScreen.jsx
+++ src/pages/MainScreen.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import MainHeader from '../header/MainHeader'
+import React from 'react';
+import MainHeader from '../header/MainHeader';
+import bodyImg from '../assets/body.svg';
 
 const MainScreen = () => {
   return (
-    <div className="min-h-screen bg-[url('/src/assets/body.svg')]  bg-center">
-    <MainHeader/>
+    <div className="relative w-screen bg-black">
+      <div className="absolute top-0 left-0 w-full z-10">
+        <MainHeader />
+      </div>
+      {/* 배경 이미지 */}
+      <img
+        src={bodyImg}
+        className="w-full h-auto block"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MainScreen
+export default MainScreen;
