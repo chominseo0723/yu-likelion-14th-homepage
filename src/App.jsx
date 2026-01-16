3de28bfe25fa80cda68167bc194dbc00bec66297
chominseo <singer100@yu.ac.kr>
2026-01-16 17:10:04 +0900

    chore : Route 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import MainScreen from "./pages/MainScreen";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div className = "font-pretendard">App</div>;
+  return(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainScreen/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
 };
 
 export default App;
