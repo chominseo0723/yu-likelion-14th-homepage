ad2f44f686bfaa97ec52bb8d4fc42c540a83b578
chominseo <singer100@yu.ac.kr>
2026-01-16 17:10:23 +0900

    feat : MainScreen 이미지 적용

---
+++ src/pages/MainScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainHeader from '../header/MainHeader'
+
+const MainScreen = () => {
+  return (
+    <div className="min-h-screen bg-[url('/src/assets/body.svg')]  bg-center">
+    <MainHeader/>
+    </div>
+  )
+}
+
+export default MainScreen
