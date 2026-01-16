0b19009bfdde2b277c3788a4c005cdcdda4f77d3
chominseo <singer100@yu.ac.kr>
2026-01-16 18:03:16 +0900

    feat : 헤더 hover시 UI 적용

--- src/header/MainHeader.jsx
+++ src/header/MainHeader.jsx
@@ -5,15 +5,18 @@ const MainHeader = () => {
     <div className='flex flex-row font-pretendard py-9 items-center justify-between'>
         <img className='pl-10' src={Logo}/>
             <div className='flex flex-row gap-10 items-center'>
-                <span className='text-[20px] text-[#686E7D] '>HOME</span>
-                <span className='text-[20px] text-[#686E7D] '>RECTUIT</span>
-                <span className='text-[20px] text-[#686E7D] '>PROJECT</span>
-                <span className='text-[20px] text-[#686E7D] mr-5'>Q&A</span>
+                <span className='text-[20px] text-[#686E7D] hover:text-[#FFAE00] hover:font-extrabold cursor-pointer'>HOME</span>
+                <span className='text-[20px] text-[#686E7D] hover:text-[#FFAE00] hover:font-extrabold cursor-pointer'>RECTUIT</span>
+                <span className='text-[20px] text-[#686E7D] hover:text-[#FFAE00] hover:font-extrabold cursor-pointer'>PROJECT</span>
+                <span className='text-[20px] text-[#686E7D] mr-5 hover:text-[#FFAE00] hover:font-extrabold cursor-pointer'>Q&A</span>
                 <div className='border rounded-[40px] px-10 py-3 font-extrabold text-white text-[20px] bg-gradient-to-r
     from-[#FF5E00]/40
     to-[#FFAE00]/40
     border-white/30
-    backdrop-blur-sm mr-15'>14기 지원하기</div>
+    backdrop-blur-sm mr-15 cursor-pointer hover:bg-gradient-to-r
+    hover:from-[#FF5E00]
+    hover:to-[#FFAE00]
+    hover:border-white/30'>14기 지원하기</div>
             </div>
         
     </div>
