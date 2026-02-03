1c01367b6fbb218d59ec92d01022b620cbb510c0
ryush77 <nex721@naver.com>
2026-02-03 16:05:28 +0900

    feature/session: 세션 데이터 분리

--- src/components/Q&A/Qna.jsx
+++ src/components/Q&A/Qna.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 import light from "../../assets/light.svg";
-import QnASection from './QnASection'
+import QnASection from "./QnASection";
 
 const Qna = () => {
   return (
@@ -9,11 +9,13 @@ const Qna = () => {
       <div className="flex flex-col ml-52">
         <div className="flex flex-row items-center gap-4">
           <img src={light} alt="light" />
-          <span className="font-normal text-[20px] bg-linear-to-r from-[#FF9000] to-[#FF5E00] 
+          <span
+            className="font-normal text-[20px] bg-linear-to-r from-[#FF9000] to-[#FF5E00] 
                     bg-clip-text text-transparent
                     [-webkit-text-stroke:0.2px_#FFAE00]
                     [text-shadow:0_1.5px_1px_rgba(124,66,5,0.9)]
-                    select-none">
+                    select-none"
+          >
             Q&A
           </span>
         </div>
@@ -23,11 +25,11 @@ const Qna = () => {
         </span>
       </div>
 
-      <div className='ml-138'>
-      <QnASection />
+      <div className="ml-138">
+        <QnASection />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Qna
+export default Qna;
