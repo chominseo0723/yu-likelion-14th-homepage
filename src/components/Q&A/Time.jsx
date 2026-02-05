00afc50c8c9624ab016716740fe7f2bb17b50186
chominseo <singer100@yu.ac.kr>
2026-02-05 15:57:37 +0900

    feat : 숫자 크기 조정

--- src/components/Q&A/Time.jsx
+++ src/components/Q&A/Time.jsx
@@ -52,7 +52,7 @@ const Time = () => {
         </p>
 
         {/* 카운트다운 */}
-        <div className="flex text-[54px]items-center gap-[28px] mb-30">
+        <div className="flex items-center gap-[28px] mb-30">
           <TimeUnit value={timeLeft.days} label="일" />
           <TimeUnit value={timeLeft.hours} label="시" />
           <TimeUnit value={timeLeft.minutes} label="분" />
@@ -105,7 +105,7 @@ backdrop-blur-xl
 border border-[rgba(255,255,255,0.25)]
               flex items-center justify-center
               text-white
-              text-[36px]
+              text-[54px]
               font-bold
             "
           >
